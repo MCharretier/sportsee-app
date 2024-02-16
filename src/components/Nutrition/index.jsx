34d09514a5e68cf4d1491a3016d6766cc49739f6
Mathis Charretier <charretiermathis@gmail.com>
2024-02-16 11:05:12 +0100

    Fix (Nutrition): add props icon, unit and name

--- src/components/Nutrition/index.jsx
+++ src/components/Nutrition/index.jsx
@@ -1,59 +1,31 @@
 import PropTypes from 'prop-types'
-import {ReactComponent as CaloriesIcon} from '../../assets/images/calories-icon.svg'
-import {ReactComponent as ProteinIcon} from '../../assets/images/protein-icon.svg'
-import {ReactComponent as CarbsIcon} from '../../assets/images/carbs-icon.svg'
-import {ReactComponent as FatIcon} from '../../assets/images/fat-icon.svg'
 import styles from './styles.module.css'
 
-function Nutrition({type, number}) {
-
-    const formatCaloriesNumber = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+function Nutrition({ icon, number, unit, name }) {
+    const formatNumber = (number) =>
+        number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 
     return (
         <div className={styles.nutrition}>
-            { type === 'calories' && (
-                <div className={styles.inner}>
-                    <CaloriesIcon className={styles.icon} />
-                    <div className={styles.content}>
-                        <span className={styles.number}>{formatCaloriesNumber(number)}kCal</span>
-                        <span className={styles.unit}>Calories</span>
-                    </div>
+            <div className={styles.inner}>
+                <img src={icon} alt={name} className={styles.icon} />
+                <div className={styles.content}>
+                    <span className={styles.number}>
+                        {formatNumber(number)}
+                        {unit}
+                    </span>
+                    <span className={styles.unit}>{name}</span>
                 </div>
-            ) }
-            { type === 'protein' && (
-                <div className={styles.inner}>
-                    <ProteinIcon className={styles.icon} />
-                    <div className={styles.content}>
-                        <span className={styles.number}>{number}g</span>
-                        <span className={styles.unit}>Proteines</span>
-                    </div>
-                </div>
-            ) }
-            { type === 'carbs' && (
-                <div className={styles.inner}>
-                    <CarbsIcon className={styles.icon} />
-                    <div className={styles.content}>
-                        <span className={styles.number}>{number}g</span>
-                        <span className={styles.unit}>Glucides</span>
-                    </div>
-                </div>
-            ) }
-            { type === 'fat' && (
-                <div className={styles.inner}>
-                    <FatIcon className={styles.icon} />
-                    <div className={styles.content}>
-                        <span className={styles.number}>{number}g</span>
-                        <span className={styles.unit}>Lipides</span>
-                    </div>
-                </div>
-            ) }
+            </div>
         </div>
     )
 }
 
 Nutrition.propTypes = {
-    type: PropTypes.string.isRequired,
-    number: PropTypes.number.isRequired
+    icon: PropTypes.any.isRequired,
+    number: PropTypes.number.isRequired,
+    unit: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
 }
 
 export default Nutrition
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { getUserInformations, getUserActivity, getUserAverageSessions, getUserPerformance } from '../../services/User'
+import {
+    getUserInformations,
+    getUserActivity,
+    getUserAverageSessions,
+    getUserPerformance,
+} from '../../services/User'
 import styles from './styles.module.css'
 import Nutrition from '../../components/Nutrition'
 import ActivityChart from '../../components/ActivityChart'
 import AverageSessionsChart from '../../components/AverageSessionsChart'
 import PerformanceChart from '../../components/PerformanceChart'
 import ScoreChart from '../../components/ScoreChart'
+import CaloriesIcon from '../../assets/images/calories-icon.svg'
+import ProteinIcon from '../../assets/images/protein-icon.svg'
+import CarbsIcon from '../../assets/images/carbs-icon.svg'
+import FatIcon from '../../assets/images/fat-icon.svg'
 
 function Dashboard() {
-
-    const { id } = useParams();
+    const { id } = useParams()
 
     const [userInformations, setUserInformations] = useState()
     const [userActivity, setUserActivity] = useState()
@@ -33,26 +41,50 @@ function Dashboard() {
 
     return (
         <div className={styles.dashboard}>
-            { userInformations && (
+            {userInformations && (
                 <div className={styles.header}>
-                    <h1>Bonjour <strong>{userInformations.firstName}</strong></h1>
-                    <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+                    <h1>
+                        Bonjour <strong>{userInformations.firstName}</strong>
+                    </h1>
+                    <p>
+                        Félicitation ! Vous avez explosé vos objectifs hier 👏
+                    </p>
                 </div>
-            ) }
-            { userInformations && (
+            )}
+            {userInformations && (
                 <div className={styles.statistics}>
                     <ActivityChart data={userActivity} />
                     <AverageSessionsChart data={userAverageSessions} />
                     <PerformanceChart data={userPerformance} />
                     <ScoreChart data={userInformations} />
                     <div className={styles.nutritions}>
-                        <Nutrition type="calories" number={userInformations.calorieCount} />
-                        <Nutrition type="protein" number={userInformations.proteinCount} />
-                        <Nutrition type="carbs" number={userInformations.carbohydrateCount} />
-                        <Nutrition type="fat" number={userInformations.lipidCount} />
+                        <Nutrition
+                            icon={CaloriesIcon}
+                            number={userInformations.calorieCount}
+                            unit="kCal"
+                            name="Calories"
+                        />
+                        <Nutrition
+                            icon={ProteinIcon}
+                            number={userInformations.proteinCount}
+                            unit="g"
+                            name="Proteines"
+                        />
+                        <Nutrition
+                            icon={CarbsIcon}
+                            number={userInformations.carbohydrateCount}
+                            unit="g"
+                            name="Glucides"
+                        />
+                        <Nutrition
+                            icon={FatIcon}
+                            number={userInformations.lipidCount}
+                            unit="g"
+                            name="Lipides"
+                        />
                     </div>
                 </div>
-            ) } 
+            )}
         </div>
     )
 }
