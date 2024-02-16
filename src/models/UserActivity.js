994bbe5c45a37f061945b0397119e2baa781bc8b
Mathis Charretier <charretiermathis@gmail.com>
2024-02-16 11:45:10 +0100

    Fix (ActivityChart): format day for XAxis

--- src/models/UserActivity.js
+++ src/models/UserActivity.js
@@ -1,8 +1,14 @@
+const getDay = (date) => {
+    const splitDate = date.split('-')
+    const day = splitDate[2]
+    return parseInt(day)
+}
+
 class UserActivity {
     constructor(data) {
         this._sessions = data.sessions.map((session) => {
             return {
-                day: session.day,
+                day: getDay(session.day),
                 kilogram: session.kilogram,
                 calories: session.calories,
             }
